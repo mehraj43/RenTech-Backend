27c7eb1532c31a4b95f3d9ffe5a1cc6e6fe81062
Pradeep <pradeepgupta36949@gmail.com>
2022-01-15 15:36:27 +0530

    Added Category functionality

--- Routers/productDetail.js
+++ Routers/productDetail.js
@@ -89,11 +89,7 @@ router.post(
 // ROUTE 2: Get product using : GET "api/productDetail/getProduct"  -Login not required
 router.get('/getProduct/:category', async (req, res) => {
   try {
-<<<<<<< HEAD
     const myProduct = await ProductDetail.find({category: req.params.category});
-=======
-    const myProduct = await ProductDetail.find({});
->>>>>>> 268cee123f0572c210e94694d27b0d457d95cec0
     res.send(myProduct);
   } catch (err) {
     res.status(500).send('Some error occured');
